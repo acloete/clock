32cfb39d3862520f874e3838b51a92a02544968a
Alistair Cloete <acloete@users.noreply.github.com>
2024-12-23 08:35:56 +0100

    add offline cache

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ function tick() {
   window.setTimeout(tick, (60-now.getSeconds())*1000 - now.getMilliseconds());
 }
 
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+  
+registerServiceWorker();  
+
 tick();
 
 if (navigator.wakeLock) {
